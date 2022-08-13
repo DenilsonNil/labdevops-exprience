3f0756e1ae49b6ab25f6d96ca5c111657e062a26
Denilson Alves Martins <30964039+DenilsonNil@users.noreply.github.com>
2022-08-13 12:43:51 -0300

    mensagem alterada

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Avanti Palestra scoppia che la vittoria é nostra"
 
 if __name__ == '__main__':
     app.run()
